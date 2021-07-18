2314cc7b95ed6996fb71291e7df5a10f810ff8be
Alex Sarnakov <a.sarnakov@gmail.com>
2021-07-18 17:16:49 +0200

    add micromodal lib

--- app/js/app.js
+++ app/js/app.js
@@ -19,4 +19,17 @@ document.addEventListener('DOMContentLoaded', () => {
 				}
 			});
 		});
+
+	MicroModal.init({
+		// onShow: (modal) => console.info(`${modal.id} is shown`),
+		// onClose: (modal) => console.info(`${modal.id} is hidden`),
+		// openTrigger: 'data-custom-open',
+		// closeTrigger: 'data-custom-close',
+		openClass: 'is-open',
+		// disableScroll: true,
+		// disableFocus: false,
+		// awaitOpenAnimation: false,
+		// awaitCloseAnimation: false,
+		// debugMode: true,
+	});
 });
